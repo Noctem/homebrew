21e1bdb71c831697a00a5afbab868245773a02f3
David Christenson <david@the-david.com>
2014-05-12 00:38:42 -0600

    berkeley-db 6.0.30
    Update to latest stable release and remove patch that is no longer
    necessary.

--- Library/Formula/berkeley-db.rb
+++ Library/Formula/berkeley-db.rb
@@ -2,23 +2,11 @@ require 'formula'
 
 class BerkeleyDb < Formula
   homepage 'http://www.oracle.com/technology/products/berkeley-db/index.html'
-  url 'http://download.oracle.com/berkeley-db/db-5.3.28.tar.gz'
-  sha1 'fa3f8a41ad5101f43d08bc0efb6241c9b6fc1ae9'
-
-  bottle do
-    cellar :any
-    sha1 "82b52acb094e7f5cfb8f807f4501b47653744285" => :mavericks
-    sha1 "32db77b55f8f864af939d842a85bed64347912e8" => :mountain_lion
-    sha1 "6846c61e17ff5dcfe25f5aaf2e7e88b71758ea09" => :lion
-  end
+  url 'http://download.oracle.com/berkeley-db/db-6.0.30.tar.gz'
+  sha1 '038dca3ef2984c6cb95ff088d1ab7c7b9da40883'
 
   option 'with-java', 'Compile with Java support.'
   option 'enable-sql', 'Compile with SQL support.'
-
-  # Fix build under Xcode 4.6
-  # Double-underscore names are reserved, and __atomic_compare_exchange is now
-  # a built-in, so rename this to something non-conflicting.
-  patch :DATA
 
   def install
     # BerkeleyDB dislikes parallel builds
@@ -47,26 +35,3 @@ class BerkeleyDb < Formula
   end
 end
 
-__END__
-diff --git a/src/dbinc/atomic.h b/src/dbinc/atomic.h
-index 096176a..561037a 100644
---- a/src/dbinc/atomic.h
-+++ b/src/dbinc/atomic.h
-@@ -144,7 +144,7 @@ typedef LONG volatile *interlocked_val;
- #define	atomic_inc(env, p)	__atomic_inc(p)
- #define	atomic_dec(env, p)	__atomic_dec(p)
- #define	atomic_compare_exchange(env, p, o, n)	\
--	__atomic_compare_exchange((p), (o), (n))
-+	__atomic_compare_exchange_db((p), (o), (n))
- static inline int __atomic_inc(db_atomic_t *p)
- {
- 	int	temp;
-@@ -176,7 +176,7 @@ static inline int __atomic_dec(db_atomic_t *p)
-  * http://gcc.gnu.org/onlinedocs/gcc-4.1.0/gcc/Atomic-Builtins.html
-  * which configure could be changed to use.
-  */
--static inline int __atomic_compare_exchange(
-+static inline int __atomic_compare_exchange_db(
- 	db_atomic_t *p, atomic_value_t oldval, atomic_value_t newval)
- {
- 	atomic_value_t was;
