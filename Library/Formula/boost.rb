2db3ba154bb706837fcc152b1df80a19fe40bcf6
David Christenson <david@the-david.com>
2014-11-03 23:09:33 -0700

    boost 1.57.0
    Update to latest stable release, change single quotes to double.

--- Library/Formula/boost.rb
+++ Library/Formula/boost.rb
@@ -1,11 +1,11 @@
-require 'formula'
+require "formula"
 
 class Boost < Formula
-  homepage 'http://www.boost.org'
-  url 'https://downloads.sourceforge.net/project/boost/boost/1.56.0/boost_1_56_0.tar.bz2'
-  sha1 'f94bb008900ed5ba1994a1072140590784b9b5df'
+  homepage "http://www.boost.org"
+  url "https://downloads.sourceforge.net/project/boost/boost/1.57.0/boost_1_57_0.tar.bz2"
+  sha1 "e151557ae47afd1b43dc3fac46f8b04a8fe51c12"
 
-  head 'https://github.com/boostorg/boost.git'
+  head "https://github.com/boostorg/boost.git"
 
   bottle do
     cellar :any
@@ -18,23 +18,23 @@ class Boost < Formula
   env :userpaths
 
   option :universal
-  option 'with-icu', 'Build regexp engine with icu support'
-  option 'without-single', 'Disable building single-threading variant'
-  option 'without-static', 'Disable building static library variant'
-  option 'with-mpi', 'Build with MPI support'
+  option "with-icu", "Build regexp engine with icu support"
+  option "without-single", "Disable building single-threading variant"
+  option "without-static", "Disable building static library variant"
+  option "with-mpi", "Build with MPI support"
   option :cxx11
 
-  if build.with? 'icu'
+  if build.with? "icu"
     if build.cxx11?
-      depends_on 'icu4c' => 'c++11'
+      depends_on "icu4c" => "c++11"
     else
-      depends_on 'icu4c'
+      depends_on "icu4c"
     end
   end
 
-  if build.with? 'mpi'
+  if build.with? "mpi"
     if build.cxx11?
-      depends_on 'open-mpi' => 'c++11'
+      depends_on "open-mpi" => "c++11"
     else
       depends_on :mpi => [:cc, :cxx, :optional]
     end
@@ -47,7 +47,7 @@ class Boost < Formula
 
   def install
     # https://svn.boost.org/trac/boost/ticket/8841
-    if build.with? 'mpi' and build.with? 'single'
+    if build.with? "mpi" and build.with? "single"
       raise <<-EOS.undent
         Building MPI support for both single and multi-threaded flavors
         is not supported.  Please use '--with-mpi' together with
@@ -60,17 +60,17 @@ class Boost < Formula
     # Force boost to compile with the desired compiler
     open("user-config.jam", "a") do |file|
       file.write "using darwin : : #{ENV.cxx} ;\n"
-      file.write "using mpi ;\n" if build.with? 'mpi'
+      file.write "using mpi ;\n" if build.with? "mpi"
     end
 
     # libdir should be set by --prefix but isn't
     bootstrap_args = ["--prefix=#{prefix}", "--libdir=#{lib}"]
 
-    if build.with? 'icu'
-      icu4c_prefix = Formula['icu4c'].opt_prefix
+    if build.with? "icu"
+      icu4c_prefix = Formula["icu4c"].opt_prefix
       bootstrap_args << "--with-icu=#{icu4c_prefix}"
     else
-      bootstrap_args << '--without-icu'
+      bootstrap_args << "--without-icu"
     end
 
     # Handle libraries that will not be built.
@@ -88,7 +88,7 @@ class Boost < Formula
     # Boost.Log cannot be built using Apple GCC at the moment. Disabled
     # on such systems.
     without_libraries << "log" if ENV.compiler == :gcc || ENV.compiler == :llvm
-    without_libraries << "mpi" if build.without? 'mpi'
+    without_libraries << "mpi" if build.without? "mpi"
 
     bootstrap_args << "--without-libraries=#{without_libraries.join(',')}"
 
@@ -129,7 +129,7 @@ class Boost < Formula
   end
 
   def caveats
-    s = ''
+    s = ""
     # ENV.compiler doesn't exist in caveats. Check library availability
     # instead.
     if Dir["#{lib}/libboost_log*"].empty?
